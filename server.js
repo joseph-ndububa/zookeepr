1194e80759e155e6e566b60bfefecaa7c7f4ff15
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-03 00:35:48 -0400

    port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const { animals } = require('./data/animals');
 const express = require('express');
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
