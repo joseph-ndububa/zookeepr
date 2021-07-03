abf1be84a373ad389423423080016e82c9ab7c32
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-03 13:32:14 -0400

    finish get requests

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -49,9 +50,6 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
@@ -84,6 +82,22 @@ function validateAnimal(animal) {
     return true;
 }
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -112,4 +126,8 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
